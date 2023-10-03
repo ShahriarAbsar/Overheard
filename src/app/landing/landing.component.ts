ba96a30509cc3d4be26f33daa7214acc5ce892d0
ShahriarAbsar <shahriarabsar786@gmail.com>
2023-10-03 20:32:54 +0600

    absar_commit

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
 
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+import { backURL } from '../interfaces/data';
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
@@ -7,4 +11,17 @@ import { Component } from '@angular/core';
 })
 export class LandingComponent {
 
+  constructor(private router: Router, private http: HttpClient) {}
+
+  email: string = '';
+
+
+  onSubmit() {
+    console.log('Email submitted:', this.email);
+    this.http.post(`${backURL}email`, {email: this.email}).subscribe(gotit => {
+      console.log(gotit);
+      this.email = '';
+      alert('Thanks we got your email!')
+    })
+  }
 }
